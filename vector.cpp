92c23f0186818f8780449e6893f94e17485eeb30
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-11-02 10:47:23 +0530

    currently learning vector from group zero and deeper

---
+++ vector.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+//============================================== Space for macro ===========================================//
+
+#define TEST_SIZE 10
+
+//=============================================== Space for user defined function ================================//
+
+
+//=============================================== space for main function =========================================//
+
+int main() {
+    std::vector<int> prime = {2,3,5,7};
+    std::vector letter = {'a', 'b'};  // C++17 feature: CTAD
+    
+	return 0;
+}
