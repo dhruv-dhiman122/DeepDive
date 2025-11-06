0bce2186684acaeeb763ae4e140167eb790c6f7a
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-11-06 16:11:44 +0530

    The smaller notes are added into the code

--- vector.cpp
+++ vector.cpp
@@ -12,7 +12,10 @@
 
 int main() {
     std::vector<int> vec = {1,2,3,4,5}; // the vector are also data type similar to classes and struct
-    
+    /*
+     * The data type are homogenous, meaning of singluar data type only
+     * The data type cant be heterogenous which is seen in python*/
+
     for(int element : vec) {
         std::cout<<element<<" ";
     }
